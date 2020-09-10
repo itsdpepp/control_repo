316e8ee22bd1e9030f9fa2f19aca4bac6edc41d2
David Pepper <d.pepper@soton.ac.uk>
2020-09-10 10:44:11 +0100

    Adds fact to readme file

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,7 +2,11 @@ node default {
   
 }
 node 'master.puppet.vm' {
-  include role::master_server
+  include role::master_server,
+  file { '/root/README':
+    ensure => file,
+    content => $fdqn,
+  }
 }
 
 node /^web/ {
